6db0c631ca1b366362992cefb08cff24af168b6b
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-02-06 19:24:45 +0100

    Update rpcmining.cpp

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -106,7 +106,7 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("currentblocktx", (uint64_t) nLastBlockTx));
     obj.push_back(Pair("difficulty", (double)GetDifficulty()));
     obj.push_back(Pair("blockvalue", (uint64_t) GetProofOfWorkReward(pindexBest->nHeight, 0)));
-    obj.push_back(Pair("netmhashps", GetPoWMHashPS()));
+    obj.push_back(Pair("networkhashps", GetPoWMHashPS()));
     obj.push_back(Pair("netstakeweight", GetPoSKernelPS()));
     obj.push_back(Pair("errors", GetWarnings("statusbar")));
     obj.push_back(Pair("pooledtx", (uint64_t) mempool.size()));
